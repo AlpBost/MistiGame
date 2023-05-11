4b875c911e518ee89853e286a326a0ad31860000
Eylul Ozturk <yjyanym@gmail.com>
2023-05-11 15:22:01 +0300

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,118 +1,65 @@
-//import java.util.random.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Deck {
-	  
-	  static private ArrayList<Card> deck= new ArrayList<Card>();
-	  static private ArrayList<Card> PlayerHand= new ArrayList<Card>();;
-	  static private ArrayList<Card> NoviceHand= new ArrayList<Card>();;
-	  static private ArrayList<Card> RegularHand= new ArrayList<Card>();;
-	  static private ArrayList<Card> ExpertHand= new ArrayList<Card>();;
-	  static private ArrayList<Card> TableCards= new ArrayList<Card>();;
-	  Scanner sc= new Scanner(System.in);
-	  
-	  //--------------------------------------------SETTERS----------------------------------------
-	  public void setDeck(ArrayList<Card> deck) {this.deck=deck;}
-	  public void setPlayerHand(ArrayList<Card> PlayerHand) {this.PlayerHand=PlayerHand;}
-	  public void setP2Hand(ArrayList<Card> NoviceHand) {this.NoviceHand=NoviceHand;}
-	  public void setP3Hand(ArrayList<Card> RegularHand) {this.RegularHand=RegularHand;}
-	  public void setP4Hand(ArrayList<Card> expertHand) {this.ExpertHand=expertHand;}
-	  public void setTableCard(ArrayList<Card> TableCards) {this.TableCards=TableCards;}
-	  
-	  //--------------------------------------------GETTERS----------------------------------------
-	  
-	  public ArrayList<Card> getDeck() {return deck;}
-	  public ArrayList<Card> getPlayerHand() {return PlayerHand;}
-	  public ArrayList<Card> getNoviceHand() {return NoviceHand;}
-	  public ArrayList<Card> getRegularHand() {return RegularHand;}
-	  public ArrayList<Card> getExpertHand() {return ExpertHand;}
-	  public ArrayList<Card> getTableCards() {return TableCards;}
-	  
-	  //-----------------------------------------CREATING A DECK------------------------------------
-	    public Deck() {
-	        // how to create a deck using an arraylist
-	        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-	        String[] suits = {"maca", "sinek", "kupa", "karo"}; /*"♠", "♣", "♥", "♦"*/
-	        
-	        // Creating an empty arraylist of Card objects.
-	        deck = new ArrayList<Card>();
+	private final ArrayList<Card> deck;
 
-	        // Populate the arraylist with Card objects.
-	        for (int count = 0; count < 52; count++)
-	            deck.add(new Card(values[count % 13], suits[count / 13]));
-	        
-	     // Shuffling the deck with the Java Collection shuffle method
-	        Collections.shuffle(deck);
-	        
-	    }
-	    
-	   
-	    //---------------------------------------------CUT-----------------------------------------
+	public Deck() {
+		deck = new ArrayList<>();
 
-	       public void Cut() {
+		// Read from the file to populate the deck with Card objects
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("pointFile.txt"));
 
-	        	Random p=new Random();
-	        	int cut=p.nextInt(deck.size());
-	        	
-	        	//create an empty arraylists to seperate the deck
-	    		ArrayList<Card> top= new ArrayList<Card>();
-	    		ArrayList<Card> bottom= new ArrayList<Card>();
-	    		
-	    		//Filling the arraylists
-	    		for (int i = 0; i < cut; i++) {
-	    			top.add(deck.get(i));
-	    		}
-	    		for (int i = cut; i < deck.size(); i++) {
-	    			bottom.add(deck.get(i));
-	    		}
-	    		
-	    	    deck.clear();
-	    		deck.addAll(bottom);
-	    	    deck.addAll(top);
-	    	    
-	    }
-	       
-	     //Deal method
-	       public void Deal() {
-	    	  System.out.print("Number of players: ");
-	    	  int players = sc.nextInt();
-	    	
-	    	  while (players<2 || players>4) { System.out.print("Enter again: ");players = sc.nextInt(); }
-	    	  if (players==2) {
-	    		  for(int i=0;i<4;i++) {
-	    			  getPlayerHand().add(deck.get(i));  deck.remove(i); 
-	    			  NoviceHand.add(deck.get(i));  deck.remove(i);
-	    		  }
-	     	  }
-	    	  if (players==3) {
-	    		  for(int i=0;i<4;i++) {
-	    			  PlayerHand.add(deck.get(i));  deck.remove(i);
-	    			  NoviceHand.add(deck.get(i));  deck.remove(i);
-	    			  RegularHand.add(deck.get(i));  deck.remove(i);
-	    			  ExpertHand.add(deck.get(i));  deck.remove(i);
+			String line;
+			while ((line = reader.readLine()) != null) {
+				// Split the line into an array of Strings
+				String[] parts = line.split(" ");
 
-	    		  }
-	    	  }
-	    	  if (players==4) {
-	    		  for(int i=0;i<4;i++) {
-	    			  getPlayerHand().add(deck.get(i));  deck.remove(i);
-	    			  getNoviceHand().add(deck.get(i));  deck.remove(i);
-	    			  RegularHand.add(deck.get(i));  deck.remove(i);
-	    			  ExpertHand.add(deck.get(i));  deck.remove(i);
-	    		  }
-	    	  }
-	       }
-       public void Display() {
-    	   //Table Cards
-	    	  for(int i=0;i<4;i++) { TableCards.add(deck.get(i)); deck.remove(i); }
-	    	  System.out.println("Top Card: " + TableCards.get(3));
-	    	
-	    	  //Player Cards
-	    	  System.out.println("Your Cards: " + PlayerHand);
+				// Extract the suit, face, and point from the parts
+				String suit = parts[0].substring(0, 1);
+				String face = parts[0].substring(1);
+				int point = Integer.parseInt(parts[1]);
 
-       }
-	
+				Card card = new Card(suit, face, point);
+				deck.add(card);
+			}
+			reader.close();
+		} catch (IOException e) {
+			System.err.println("Error reading from file: " + e.getMessage());
+		}
+
+		Collections.shuffle(deck); // Shuffle the deck
+
+		// Cut the deck
+		Random random = new Random(); // a Random object to generate a random place to cut
+		int x = random.nextInt(deck.size());
+
+		ArrayList<Card> top = new ArrayList<>();
+		ArrayList<Card> bottom = new ArrayList<>();
+
+		for (int i = 0; i < x; i++)
+			top.add(deck.get(i));
+
+		for (int i = x; i < deck.size(); i++)
+			bottom.add(deck.get(i));
+
+		deck.clear();
+		deck.addAll(bottom);
+		deck.addAll(top);
+	}
+
+	public ArrayList<Card> deal() {
+		// Draw 4 cards from the top of the deck
+		ArrayList<Card> tempHand = new ArrayList<>();
+		for (int i = 0 ; i < 4 ; i++) {
+			tempHand.add(deck.get(deck.size() - 1));
+			deck.remove(deck.size() - 1);
+		}
+		return tempHand;
+	}
 }
