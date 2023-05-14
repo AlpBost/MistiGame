2de821c4d55631e74d1587a383767dd1447bb080
Eylul Ozturk <yjyanym@gmail.com>
2023-05-14 13:57:07 +0300

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -4,39 +4,56 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Deck {
 	private final ArrayList<Card> deck;
 
 	public Deck() {
+		Scanner scanner = new Scanner(System.in);
 		deck = new ArrayList<>();
 
-		// Read from the file to populate the deck with Card objects
+		// Create cards with each possible suit and face.
+		String[] suits = {"♠", "♣", "♥", "♦"};
+		String[] faces = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		int point = 1; // default point
+
+		for (String suit : suits) {
+			for (String face : faces) {
+				Card card = new Card(suit, face, point);
+				deck.add(card);
+			}
+		}
+
+		// Assign points to the cards from the pointFile.
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader("pointFile.txt"));
+			System.out.println("Enter the path of the point file as following format: fileName.txt");
+			BufferedReader reader = new BufferedReader(new FileReader(scanner.nextLine()));
 
 			String line;
 			while ((line = reader.readLine()) != null) {
 				// Split the line into an array of Strings
 				String[] parts = line.split(" ");
 
-				// Extract the suit, face, and point from the parts
-				String suit = parts[0].substring(0, 1);
-				String face = parts[0].substring(1);
-				int point = Integer.parseInt(parts[1]);
+				String face = parts[0];
+				point = Integer.parseInt(parts[1]);
 
-				Card card = new Card(suit, face, point);
-				deck.add(card);
+				Card card = new Card("", face, point);
+				for (int i = 0 ; i < deck.size() ; i++) {
+					Card tempCard = deck.get(i);
+					if(tempCard.getFace().equals(face))
+						deck.set(i, card);
+				}
 			}
 			reader.close();
-		} catch (IOException e) {
-			System.err.println("Error reading from file: " + e.getMessage());
+		} catch (IOException ioException) {
+			System.err.println("Error reading from file: " + ioException.getMessage());
 		}
 
-		Collections.shuffle(deck); // Shuffle the deck
+		Collections.shuffle(deck);
 
 		// Cut the deck
-		Random random = new Random(); // a Random object to generate a random place to cut
+		Random random = new Random();
 		int x = random.nextInt(deck.size());
 
 		ArrayList<Card> top = new ArrayList<>();
@@ -51,6 +68,8 @@ public class Deck {
 		deck.clear();
 		deck.addAll(bottom);
 		deck.addAll(top);
+
+		//System.out.println(deck);
 	}
 
 	public ArrayList<Card> deal() {
